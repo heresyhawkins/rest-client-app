b6954d4d14a17c03e13d98e66e6cc6d34ee082fa
heresyhawkins <heresyhawkins@gmail.com>
2025-09-03 15:05:29 +0300

    feat: add structure folder in project

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export * from './ui/logo/Logo.tsx';
-export * from './ui/external-link/ExternalLink.tsx';
-export * from './ui/footer/Footer.tsx';
+export * from "./Logo/Logo.tsx";
+export * from "./ExternalLink/ExternalLink.tsx";
+export * from "./Footer/Footer.tsx";
--- src/components/ui/logo/Logo.tsx
+++
@@ -1,6 +0,0 @@
-import type {FC} from "react";
-
-export const Logo: FC<{src: string, alt: string}> = ({src, alt}) => {
-    return <img className="max-h-10 w-5" src={src} alt={alt}/>
-};
-
